644fc039983566768230c8aec174f208386f039d
shigeru0410 <pp3sr4@gmail.com>
2020-08-12 22:46:03 +0900

    バリデーション追加

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,6 +3,15 @@ class Message < ApplicationRecord
   belongs_to :user
 
   validates :content, presence: true, unless: :image?
+  # validates :content, exclusion: { in: %w(です よろしくお願い致します) }
+  validate :data_cannot
 
+  def data_cannot
+    text = ["です", "ました", "ます"]
+    if text.any? { |t| content.include?(t) }
+      errors.add(:base)
+    end
+  end
+  
   mount_uploader :image, ImageUploader
 end
