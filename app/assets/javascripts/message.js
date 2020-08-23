ded1ec1c3031a3b061eaedd4a03cf52b8b8916ac
shigeru0410 <pp3sr4@gmail.com>
2020-08-23 23:25:09 +0900

    メッセージ送信を非同期通信

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,5 +1,44 @@
 $(function () {
-  $(".Form").on("submit", function (e) {
+  function buildHTML(message) {
+    if (message.image) {
+      let html = `<div class="middle-items" data-message-id=${message.id}>
+          <div class="middle-items__info">
+            <div class="middle-items__info__name">
+              ${message.user_name}
+            </div>
+            <div class="middle-items__info__date">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="middle-items__message">
+            <p class="middle-items__message__text">
+              ${message.content}
+            </p>
+            <img class="middle-items__message__image" src="${message.image}">
+          </div>
+        </div>`;
+      return html;
+    } else {
+      let html = `<div class="middle-items" data-message-id=${message.id}>
+        <div class="middle-items__info">
+          <div class="middle-items__info__name">
+            ${message.user_name}
+          </div>
+          <div class="middle-items__info__date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="middle-items__message">
+          <p class="middle-items__message__text">
+            ${message.content}
+          </p>
+        </div>
+      </div>`;
+      return html;
+    }
+  }
+
+  $(".bottom-items").on("submit", function (e) {
     e.preventDefault();
     let formData = new FormData(this);
     let url = $(this).attr("action");
@@ -10,6 +49,19 @@ $(function () {
       dataType: "json",
       processData: false,
       contentType: false,
-    });
+    })
+      .done(function (data) {
+        let html = buildHTML(data);
+        $(".chat-main__message-list").append(html);
+        $("form")[0].reset();
+        $(".chat-main__message-list").animate({
+          scrollTop: $(".chat-main__message-list")[0].scrollHeight,
+        });
+        $(".bottom-items__submitbtn").prop("disabled", false);
+      })
+      .fail(function () {
+        alert("メッセージ送信に失敗しました");
+        $(".bottom-items__submitbtn").prop("disabled", false);
+      });
   });
 });
