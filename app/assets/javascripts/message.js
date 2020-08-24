20eda8e0fe779b32ffb954071935e35687a92cfa
shigeru0410 <pp3sr4@gmail.com>
2020-08-24 15:40:00 +0900

    自分の投稿は右側に

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,6 +2,7 @@ $(function () {
   function buildHTML(message) {
     if (message.image) {
       let html = `<div class="middle-items" data-message-id=${message.id}>
+        <div class="middle-items__right">
           <div class="middle-items__info">
             <div class="middle-items__info__name">
               ${message.user_name}
@@ -16,22 +17,25 @@ $(function () {
             </p>
             <img class="middle-items__message__image" src="${message.image}">
           </div>
-        </div>`;
+        </div>
+      </div>`;
       return html;
     } else {
       let html = `<div class="middle-items" data-message-id=${message.id}>
-        <div class="middle-items__info">
-          <div class="middle-items__info__name">
-            ${message.user_name}
+        <div class="middle-items__right">
+          <div class="middle-items__info">
+            <div class="middle-items__info__name">
+              ${message.user_name}
+            </div>
+            <div class="middle-items__info__date">
+              ${message.created_at}
+            </div>
           </div>
-          <div class="middle-items__info__date">
-            ${message.created_at}
+          <div class="middle-items__message">
+            <p class="middle-items__message__text">
+              ${message.content}
+            </p>
           </div>
-        </div>
-        <div class="middle-items__message">
-          <p class="middle-items__message__text">
-            ${message.content}
-          </p>
         </div>
       </div>`;
       return html;
